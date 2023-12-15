9ed27876aa3a0a09f22ac6a02a9b8f9f9b4c5a62
paul-vdb <paul.vandambates@gmail.com>
2023-12-15 09:07:24 -0800

    minor change from discussion today

--- ChandlerPaper/VisualizeProcess.r
+++ ChandlerPaper/VisualizeProcess.r
@@ -15,7 +15,8 @@ rho <- 0.952
 beta <- -log(rho)
 sigma <- exp(6.49)
 lambda <- 0.952 #per hour
-sigma_sm <- 26.78
+sigma_sm <- 26.78   ## From paper sigma_det
+# sigma_sm <- sqrt(sigma^2*(1-rho^2))   ## What Paul would do (but without telemetry data).
 Time <- 24 # One day simulated path.
 s <- c(0,0)
 mu <- simulate$simPath(n = Time, s = s, sigma = sigma, beta = beta, deltaT = 1)
@@ -29,6 +30,9 @@ H.chand <- simulate$calcChandlerPath(x = grd, mu = mu[,1:2], deltaT = 1,
 dat <- data.frame(grd, H = H.chand)							
 
 ## Visualize what happens
+## This is a plot of the sum of the daily intensity from a half-normal at each mask point conditional 
+## on the animal's location for that hour.
 ggplot(data = dat, aes(x=x, y=y)) + 
 	geom_tile(aes(fill = H)) + 
-	geom_path(data = data.frame(mu), aes(x=X1, y=X2), col = 'red')
+	geom_path(data = data.frame(mu), aes(x=X1, y=X2), col = 'red') + 
+  geom_point(data = data.frame(mu), aes(x=X1, y=X2), col = 'black')
